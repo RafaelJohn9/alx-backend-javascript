5889b60049416f1a89506ac4e1e6be6176c01400
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-03 19:57:29 +0300

    11.iterator

---
+++ 0x00-ES6_basic/dist/11-createEmployeesObject.dev.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = createEmployeesObject;
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+function createEmployeesObject(departmentName, employees) {
+  return _defineProperty({}, "".concat(departmentName), _toConsumableArray(employees));
+}
---
+++ 0x00-ES6_basic/dist/11-main.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _createEmployeesObject = _interopRequireDefault(require("./11-createEmployeesObject.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+console.log((0, _createEmployeesObject["default"])("Software", ["Bob", "Sylvie"]));
