4fb540884bc5c116e163bc7ef939012cdcd7a894
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-03 18:14:30 +0300

    4.Rest parameter syntax for functions

---
+++ 0x00-ES6_basic/dist/4-main.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _restParameter = _interopRequireDefault(require("./4-rest-parameter.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+console.log((0, _restParameter["default"])("one"));
+console.log((0, _restParameter["default"])("one", "two", 3, "4th"));
---
+++ 0x00-ES6_basic/dist/4-rest-parameter.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = returnHowManyArguments;
+
+function returnHowManyArguments() {
+  return arguments.length;
+}
