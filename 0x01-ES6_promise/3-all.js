6007cadce9ed1594fbbdd7de22b5ce732053436e
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-04 05:22:55 +0300

    3.Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,2 +1,24 @@
-const uploadPhoto = require("./utils.js").uploadPhoto;
-const creatUser = require("./utils.js").createUser;
+/* eslint-disable import/extensions */
+const { uploadPhoto } = require('./utils.js');
+const { createUser } = require('./utils.js');
+
+function handleProfileSignup() {
+  const photoUpdate = uploadPhoto();
+  photoUpdate
+    .then((response) => {
+      process.stdout.write(response.body);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+
+  const user = createUser();
+  user
+    .then((response) => {
+      console.log(` ${response.firstName} ${response.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+export default handleProfileSignup;
