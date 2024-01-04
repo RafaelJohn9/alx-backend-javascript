18da904ce65693694e8e6fcbcc059baf2b9a9610
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-04 03:14:06 +0300

    0.fixed creation of promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,6 @@
 function getResponseFromAPI() {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      const data = { message: 'Hello from the API!' };
-      resolve(data);
-    }, 1000);
+    resolve(true);
   });
 }
-
 export default getResponseFromAPI;
