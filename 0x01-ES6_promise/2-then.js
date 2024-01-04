657cfee54be8f76e74115f22cc33ef11aab045da
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-04 04:52:15 +0300

    2.catch me if you can

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      throw new Error();
+    });
+}
+export default handleResponseFromAPI;
