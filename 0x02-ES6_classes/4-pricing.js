2e168b9480256911da58f3678542f21acc8bccaf
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-04 19:16:09 +0300

    4.pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+/* eslint-disable class-methods-use-this */
+import Currency from './3-currency';
+
+export default class Price {
+  constructor(amount, currency) {
+    this._amount = this.validateNumber(amount);
+    this._currency = this.validateCurrency(currency);
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = this.validateNumber(newAmount);
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = this.validateCurrency(newCurrency);
+  }
+
+  validateNumber(number) {
+    if (typeof number !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else {
+      return (number);
+    }
+  }
+
+  validateCurrency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    } else {
+      return (currency);
+    }
+  }
+
+  displayFullPrice() {
+    return (`${this._amount} ${this._currency.displayFullCurrency()}`);
+  }
+}
