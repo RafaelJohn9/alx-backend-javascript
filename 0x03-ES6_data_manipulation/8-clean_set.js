b325446d88bbc30aa0b61c61a4e1b61646169cf0
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-08 22:08:20 +0300

    8.clean Set correction of additional of parameters

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,14 @@
+/* eslint-disable no-continue */
 export default function cleanSet(set, startString) {
   let resultString = '';
   let firstMatch = true;
-
+  if (startString === 0 || set.size === 0) {
+    return resultString;
+  }
   for (const value of set) {
+    if (value.length < startString.length) {
+      continue;
+    }
     if (value.startsWith(startString)) {
       if (!firstMatch) {
         resultString += '-';
