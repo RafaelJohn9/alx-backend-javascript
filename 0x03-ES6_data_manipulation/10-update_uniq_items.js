72397df0bc5eb71d7e847bc664bea2e1ffbf787d
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-08 22:32:14 +0300

    10.fixed eslint

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,10 @@
-export default function updateUniqueItems(map){
-	if (!(map  instanceof Map)){
-		throw new Error("Cannot process");
-	}
-	for (const [item, quantity] of map.entries()){
-		if(quantity === 1){
-			map.set(item, 100);
-		}
-	}
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  for (const [item, quantity] of map.entries()) {
+    if (quantity === 1) {
+      map.set(item, 100);
+    }
+  }
 }
