99c77ef95b03f6acbfeb47d0c9927f5265028959
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-08 21:54:40 +0300

    8.fixed it

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,18 +1,6 @@
 export default function cleanSet(set, startString) {
-  let newString = '';
-  let firstString = true;
-
-  if (startString.length === 0 || set.size === 0) {
-    return newString;
-  }
-  for (const string of set) {
-    if (string.slice(0, startString.length) === startString) {
-      if (!firstString) {
-        newString += '-';
-      }
-      newString += string.slice(startString.length);
-      firstString = false;
-    }
-  }
-  return newString;
+  return Array.from(set)
+    .filter((element) => element.startsWith(startString) && startString.length > 0)
+    .map((element) => element.replace(startString, ''))
+    .join('-');
 }
