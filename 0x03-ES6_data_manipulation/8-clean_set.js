861e616bf609646d44885b4cbf0de177dba5ec54
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-08 21:42:18 +0300

    8.clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,18 @@
+export default function cleanSet(set, startString) {
+  let newString = '';
+  let firstString = true;
+
+  if (startString.length === 0) {
+    return newString;
+  }
+  for (const string of set) {
+    if (string.slice(0, startString.length) === startString) {
+      if (!firstString) {
+        newString += '-';
+      }
+      newString += string.slice(startString.length);
+      firstString = false;
+    }
+  }
+  return newString;
+}
