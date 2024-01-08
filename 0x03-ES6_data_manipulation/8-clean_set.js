e503e08ea0abed86fa5f8436033f1a00a6a654e0
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-08 22:00:40 +0300

    8.correcting code

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,16 @@
-export default function cleanSet(set, startString) {
-  return Array.from(set)
-    .filter((element) => element.startsWith(startString) && startString.length > 0)
-    .map((element) => element.replace(startString, ''))
-    .join('-');
+function cleanSet(set, startString) {
+  let resultString = '';
+  let firstMatch = true;
+
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      if (!firstMatch) {
+        resultString += '-';
+      }
+      resultString += value.slice(startString.length);
+      firstMatch = false;
+    }
+  }
+
+  return resultString;
 }
