dfd362d1f821c319c660ed3372f3f6c7be9eecf2
RafaelJohn9 <rafaeljohb@gmail.com>
2024-01-08 21:06:27 +0300

    5.fixed eslint

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,14 @@
-export default function updateStudentGradeByCity(students, city, newGrades){
-	return students
-	.filter(student => student.location === city)
-	.map(student => {
-		const matchingGrade = newGrades.find(grade => grade.studentid === student.id);
-		
-		return {
-			id: student.id,
-			firstName: student.firstName,
-			location: student.location,
-			grade: matchingGrade ? matchingGrade.grade: 'N/A'
-		}
-	});
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find((grade) => grade.studentid === student.id);
+
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: matchingGrade ? matchingGrade.grade : 'N/A',
+      };
+    });
 }
