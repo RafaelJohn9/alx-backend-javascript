ad3f42520b53260f4e04461351340d5a73f04e2f
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-28 00:40:35 +0300

    4. Create a small HTTP server using node's http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * used to create a http server
+ */
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School');
+});
+const port = 1245;
+server.listen(port);
