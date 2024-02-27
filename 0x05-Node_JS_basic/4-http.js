e1bc8987e2719aec4cdcabcaf908064439afd368
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-28 00:42:19 +0300

    corrected output: added exclamation mark to endpoint return value

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,7 +6,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School');
+  res.end('Hello Holberton School!');
 });
 const port = 1245;
 server.listen(port);
