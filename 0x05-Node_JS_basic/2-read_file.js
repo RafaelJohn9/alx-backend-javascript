0e11f6134505e510fd82d429483963b307fa41b8
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-27 23:06:37 +0300

    2. Reading a file sychronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n');
+    const students = {};
+    let firstLine = true;
+    for (const line of lines) {
+      if (firstLine) {
+        firstLine = false;
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+      if (line !== '') {
+        const student = line.split(',');
+        if (!students[student[3]]) {
+          students[student[3]] = [];
+        }
+        students[student[3]].push(student[0]);
+      }
+    }
+    console.log(`Number of students: ${lines.length - 1}`);
+    for (const field in students) {
+      if (field) {
+        console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
