38be3793c4cb18fb2c1170fda21c7226d115a948
RafaelJohn9 <rafaeljohb@gmail.com>
2024-03-01 05:08:36 +0300

    1.combining descriptions

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,23 +1,17 @@
-function calculateNumber(type, a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-        throw new Error('Parameters must be numbers');
+function calculateNumber (type, a, b){
+    a = Math.round(a);
+    b = Math.round(b);
+    if (type === 'SUM'){
+	    return (a + b);
     }
-
-    const roundedA = Math.round(a);
-    const roundedB = Math.round(b);
-
-    switch (type) {
-        case 'SUM':
-            return roundedA + roundedB;
-        case 'SUBTRACT':
-            return roundedA - roundedB;
-        case 'DIVIDE':
-            if (roundedB === 0) {
-                return 'Error';
-            }
-            return roundedA / roundedB;
-        default:
-            throw new Error('Invalid type. Type must be SUM, SUBTRACT, or DIVIDE.');
+    else if (type === 'SUBTRACT'){
+	    return (a - b);
+    }
+    else if(type === 'DIVIDE'){
+    if (b === 0){
+	    return ("Error");
+    }
+    return (a / b);
     }
 }
 
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,70 +1,26 @@
-const calculateNumber = require('./1-calcul')
-const assert = require('assert')
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
 
-describe('calculate', () => {
-    describe("SUM", () => {
-        it('should add two positive numbers correctly', () => {
-            assert.equal(calculateNumber("SUM", 3, 5), 8)
-        })
-        it('test it with floating first var', () => {
-            assert.strictEqual(calculateNumber("SUM", 1.0, 3), 4);
-            assert.strictEqual(calculateNumber("SUM", 1.2, 3), 4);
-            assert.strictEqual(calculateNumber("SUM", 1.7, 3), 5);
-        })
-        it('test it with floating second var', () => {
-            assert.strictEqual(calculateNumber("SUM", 1, 2.0), 3)
-            assert.strictEqual(calculateNumber("SUM", 1, 2.6), 4)
-            assert.strictEqual(calculateNumber("SUM", 1, 2.2), 3)
-        })
-        it('test it with floating second var', () => {
-            assert.strictEqual(calculateNumber("SUM", 1.9, 2.0), 4)
-            assert.strictEqual(calculateNumber("SUM", 1.9, 2.6), 5)
-            assert.strictEqual(calculateNumber("SUM", 1.2, 2.6), 4)
-        })
-        it('test it with neagive floating var', () => {
-            assert.strictEqual(calculateNumber("SUM", -2.0, 1.9), 0)
-            assert.strictEqual(calculateNumber("SUM", 2.6, -1.9), 1)
-            assert.strictEqual(calculateNumber("SUM", -2.6, -1.2), -4)
-        })
-    })
-    describe("sub", () => {
-        it('should add two positive numbers correctly', () => {
-            assert.equal(calculateNumber("SUBTRACT", 5, 3), 2)
-        })
-        it('test it with floating first var', () => {
-            assert.strictEqual(calculateNumber("SUBTRACT", 3, 1.0), 2);
-            assert.strictEqual(calculateNumber("SUBTRACT", 3, 1.2), 2);
-            assert.strictEqual(calculateNumber("SUBTRACT", 3, 1.7), 1);
-        })
-        it('test it with floating second var', () => {
-            assert.strictEqual(calculateNumber("SUBTRACT", 2.0, 1), 1)
-            assert.strictEqual(calculateNumber("SUBTRACT", 2.6, 1), 2)
-            assert.strictEqual(calculateNumber("SUBTRACT", 2.2, 1), 1)
-        })
-        it('test it with floating second var', () => {
-            assert.strictEqual(calculateNumber("SUBTRACT", 2.0, 1.9), 0)
-            assert.strictEqual(calculateNumber("SUBTRACT", 2.6, 1.9), 1)
-            assert.strictEqual(calculateNumber("SUBTRACT", 2.6, 1.2), 2)
-        })
-        it('test it with neagive floating var', () => {
-            assert.strictEqual(calculateNumber("SUBTRACT", -2.0, 1.9), -4)
-            assert.strictEqual(calculateNumber("SUBTRACT", 2.6, -1.9), 5)
-            assert.strictEqual(calculateNumber("SUBTRACT", -2.6, -1.2), -2)
-        })
-    })
-    describe('DIVIDE', () => {
-        it("postive full numbers", () => {
-            assert.equal(calculateNumber("DIVIDE", 8.0, 2.0), 4)
-        })
-        it("negative full numbers", () => {
-            assert.equal(calculateNumber("DIVIDE", -8.0, 2.0), -4)
-            assert.equal(calculateNumber("DIVIDE", -8.0, -2.0), 4)
-            assert.equal(calculateNumber("DIVIDE", 8.0, -2.0), -4)
-            assert.equal(calculateNumber("DIVIDE", 9.0, -2.0), -4.5)
-        })
-        it("dividing by negative num full numbers", () => {
-            assert.equal(calculateNumber("DIVIDE", -8.0, 0), "Error")
-            assert.equal(calculateNumber("DIVIDE", 8.0, 0), "Error")
-        })
-    })
-})
+describe("tests the calculateNumber function", () =>{
+	it ('tests for addition mixed cases' () =>{
+		assert.strictEqual(calculateNumber('SUM', 10, 10), 20);
+		assert.strictEqual(calculateNumber('SUM', 10.5, 11), 22);
+		assert.strictEqual(calculateNumber('SUM', 10.5, 13.5), 25);
+		assert.strictEqual(calculateNumber('SUM', 3, 1.3), 4);
+	});
+
+	it ('tests for addition mixed cases' () =>{
+		assert.strictEqual(calculateNumber('DIVIDE', 10, 10), 1);
+		assert.strictEqual(calculateNumber('DIVIDE', 10.5, 0), 'Error');
+		assert.strictEqual(calculateNumber('DIVIDE', 10, 5), 2);
+		assert.strictEqual(calculateNumber('DIVIDE', 3, 1.3), 1.5);
+	});
+
+	it ('tests for addition mixed cases' () =>{
+		assert.strictEqual(calculateNumber('SUBTRACT', 10, 10), 0);
+		assert.strictEqual(calculateNumber('SUBTRACT', 10.5, 11), 0);
+		assert.strictEqual(calculateNumber('SUBTRACT', 10.5, 13.5), -3);
+		assert.strictEqual(calculateNumber('SUBTRACT', 3, 1.3), 2);
+	});
+
+});
