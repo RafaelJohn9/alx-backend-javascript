5323032fcfcda5b0a311899ac7a017cfa78abf73
RafaelJohn9 <rafaeljohb@gmail.com>
2024-03-01 05:19:02 +0300

    1.corrected errors in package.json

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,21 +2,21 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe("tests the calculateNumber function", () =>{
-	it ('tests for addition mixed cases' () =>{
+	it ('tests for addition mixed cases', () =>{
 		assert.strictEqual(calculateNumber('SUM', 10, 10), 20);
 		assert.strictEqual(calculateNumber('SUM', 10.5, 11), 22);
 		assert.strictEqual(calculateNumber('SUM', 10.5, 13.5), 25);
 		assert.strictEqual(calculateNumber('SUM', 3, 1.3), 4);
 	});
 
-	it ('tests for addition mixed cases' () =>{
+	it ('tests for divide mixed cases', () =>{
 		assert.strictEqual(calculateNumber('DIVIDE', 10, 10), 1);
 		assert.strictEqual(calculateNumber('DIVIDE', 10.5, 0), 'Error');
 		assert.strictEqual(calculateNumber('DIVIDE', 10, 5), 2);
-		assert.strictEqual(calculateNumber('DIVIDE', 3, 1.3), 1.5);
+		assert.strictEqual(calculateNumber('DIVIDE', 3, 1.3), 3);
 	});
 
-	it ('tests for addition mixed cases' () =>{
+	it ('tests for subtraction mixed cases', () =>{
 		assert.strictEqual(calculateNumber('SUBTRACT', 10, 10), 0);
 		assert.strictEqual(calculateNumber('SUBTRACT', 10.5, 11), 0);
 		assert.strictEqual(calculateNumber('SUBTRACT', 10.5, 13.5), -3);
