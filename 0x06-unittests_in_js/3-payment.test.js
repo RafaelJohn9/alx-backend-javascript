94f4e5ab329bab05756f3f53ddc62bbdb7708d79
RafaelJohn9 <rafaeljohb@gmail.com>
2024-03-02 16:17:44 +0300

    added more tests

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,20 +5,27 @@ const Utils = require('./utils')
 
 describe("tests if the request sent to the API is correct", ()=>{
 	it("Should log the correct outputs to the console", () => {
-		let consoleLogSpy = sinon.spy(console, 'log');
+		const consoleLogSpy = sinon.spy(console, 'log');
 		sendPaymentRequestToApi(1000, 50);
 		assert.strictEqual(consoleLogSpy.firstCall.args[0], `The total is: ${Utils.calculateNumber('SUM', 1000, 50)}`)
 		consoleLogSpy.restore();
-		consoleLogSpy = sinon.spy(console, 'log');
+	});
+	it("Should log the correct outputs to the console", () => {
+		const consoleLogSpy = sinon.spy(console, 'log');
 		sendPaymentRequestToApi(2000, 500);
 		assert.strictEqual(consoleLogSpy.firstCall.args[0], `The total is: ${Utils.calculateNumber('SUM', 2000, 500)}`)
 		consoleLogSpy.restore();
-		consoleLogSpy = sinon.spy(console, 'log');
+	});
+	it("Should log the correct outputs to the console", () => {
+		const consoleLogSpy = sinon.spy(console, 'log');
 		sendPaymentRequestToApi(100, 50);
 		assert.strictEqual(consoleLogSpy.firstCall.args[0], `The total is: ${Utils.calculateNumber('SUM', 100, 50)}`)
 		consoleLogSpy.restore();
-		consoleLogSpy = sinon.spy(console, 'log');
+	});
+	it("Should log the correct outputs to the console", () => {
+		const consoleLogSpy = sinon.spy(console, 'log');
 		sendPaymentRequestToApi(200, 50);
 		assert.strictEqual(consoleLogSpy.firstCall.args[0], `The total is: ${Utils.calculateNumber('SUM', 200, 50)}`)
+		consoleLogSpy.restore();
 	});
 })
