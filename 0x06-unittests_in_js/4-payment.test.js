f27a948dbe1a3255cf89d3546c0678318182ee53
RafaelJohn9 <rafaeljohb@gmail.com>
2024-03-03 06:01:53 +0300

    4.stubs stopped using expect

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,55 +1,25 @@
 const sinon = require('sinon');
 const assert = require('assert');
-const { expect } = require('chai');
+const { expect } = import('chai');
 const sendPaymentRequestToApi = require('./3-payment')
 const Utils = require('./utils')
 
 describe("tests if the request sent to the API is correct", ()=>{
-	it("Should log the correct outputs to the console", () => {
-		const consoleLogSpy = sinon.spy(console, 'log');
-		sendPaymentRequestToApi(1000, 50);
-		assert.strictEqual(consoleLogSpy.firstCall.args[0], `The total is: ${Utils.calculateNumber('SUM', 1000, 50)}`)
-		consoleLogSpy.restore();
-	});
-	it("Should log the correct outputs to the console", () => {
-		const consoleLogSpy = sinon.spy(console, 'log');
-		sendPaymentRequestToApi(2000, 500);
-		assert.strictEqual(consoleLogSpy.firstCall.args[0], `The total is: ${Utils.calculateNumber('SUM', 2000, 500)}`)
-		consoleLogSpy.restore();
-	});
-	it("Should log the correct outputs to the console", () => {
-		const consoleLogSpy = sinon.spy(console, 'log');
-		sendPaymentRequestToApi(100, 50);
-		assert.strictEqual(consoleLogSpy.firstCall.args[0], `The total is: ${Utils.calculateNumber('SUM', 100, 50)}`)
-		consoleLogSpy.restore();
-	});
-	it("Should log the correct outputs to the console", () => {
-		const consoleLogSpy = sinon.spy(console, 'log');
-		sendPaymentRequestToApi(200, 50);
-		assert.strictEqual(consoleLogSpy.firstCall.args[0], `The total is: ${Utils.calculateNumber('SUM', 200, 50)}`)
-		consoleLogSpy.restore();
-	});
-	it('should call Utils.calculateNumber with SUM type and correct arguments', () => {
-		const calculateNumberSpy = sinon.spy(Utils);
-		sendPaymentRequestToApi(100, 20);
-		expect(calculateNumberSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-		expect(calculateNumberSpy.calculateNumber.callCount).to.be.equal(1);
-		calculateNumberSpy.calculateNumber.restore();
-	});
-
 	it("Stubbing utils.calculateNumber to return a certain value", () =>{
-		const stub = sinon.stub(Utils, "calculateNumber").returns(10);
-		const consoleLogSpy = sinon.spy(console, "log");
+		const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+		const consoleSpy = sinon.spy(console, 'log');
+
 		sendPaymentRequestToApi(100, 20);
 
-		expect(stub.assert.calledWithExactly("SUM", 100, 20)).to.be.true;
-		expect(stub.callCount).to.be.equal(1);
+		assert.strictEqual(stub.calledWithExactly('SUM', 100, 20), true);
+		assert.strictEqual((stub.callCount), 1);
 
-		assert.strictEqual(consoleLogSpy.firstCall.args[0], `The total is: 10`)
-		expect(consoleLogSpy.callCount).to.be.equal(1);
+		assert.strictEqual(consoleSpy.calledWithExactly('The total is: 10'), true);
+		sinon.assert.calledOnce(consoleSpy);
 
 		stub.restore();
-		consoleLogSpy.restore();
+		consoleSpy.restore();
+
 	});
 
 });
